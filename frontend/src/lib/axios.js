018b00e55c754fe0e19579f7b954844c4c6ed29b
ABHINANDAN9905 <kumarabhinandan1307@gmail.com>
2026-06-08 03:53:45 +0530

    Fix CORS

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  (import.meta.env.MODE === "development" ? "http://localhost:5005/api" : "/api");
+  (import.meta.env.MODE === "development" ? "https://connectly-fo5l.onrender.com" : "/api");
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
