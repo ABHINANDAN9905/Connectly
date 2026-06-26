d9bde1f787461a4876431822a3ff1c0d2a767ef9
ABHINANDAN9905 <kumarabhinandan1307@gmail.com>
2026-06-26 12:49:55 +0530

    Add groups feature backend

--- Backend/src/models/group.js
+++ Backend/src/models/group.js
@@ -1,30 +1,37 @@
 import mongoose from "mongoose";
+
 const groupSchema = new mongoose.Schema(
-{
-  name: {
-    type: String,
-    required: true,
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    }],
+    streamChannelId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  description: {
-    type: String,
-    default: "",
-  },
-  avatar: {
-    type: String,
-    default: "",
-  },
-  admin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  members: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  }],
-},
-{
-  timestamps: true,
-}
+  {
+    timestamps: true,
+  }
 );
+
 export default mongoose.model("Group", groupSchema);
