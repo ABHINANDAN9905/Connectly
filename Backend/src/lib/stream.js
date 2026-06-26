7293f85a1f68277de871183a1f5070c14d4a04d7
ABHINANDAN9905 <kumarabhinandan1307@gmail.com>
2026-06-26 13:17:27 +0530

    Export streamClient from stream.js

--- Backend/src/lib/stream.js
+++ Backend/src/lib/stream.js
@@ -1,11 +1,15 @@
 import { StreamChat } from "stream-chat";
 import "dotenv/config";
+
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
+
 if (!apiKey || !apiSecret) {
   console.error("Stream API key or Secret is missing");
 }
-const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
 export const upsertStreamUser = async (userData) => {
   try {
     await streamClient.upsertUsers([userData]);
@@ -14,6 +18,7 @@ export const upsertStreamUser = async (userData) => {
     console.error("Error upserting Stream user:", error);
   }
 };
+
 export const generateStreamToken = (userId) => {
   try {
     const userIdStr = userId.toString();
